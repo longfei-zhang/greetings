d1b45f43b8e3a11fbf8994e42264719be4899c93
longfei.zhang <longfei.zhang@easystack.cn>
2021-11-17 15:22:21 +0800

    Add check name logic and return error if needed

--- greetings.go
+++ greetings.go
@@ -1,12 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("pls input your name first")
+	}
 	//Return a greeting that embeds the name in a message.
 	// var message string
 	// message = fmt.Sprintf("Hi , %v. Welcome!", name)
 	message := fmt.Sprintf("Hi , %v. Welcome!", name)
-	return message
+	return message, nil
 }
